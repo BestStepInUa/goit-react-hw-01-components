f10031843532becefebaca51748e2794bc5f8124
BestStepInUa <beststep.in.ua@gmail.com>
2023-11-27 00:46:09 +0200

    task1_done

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,18 +1,19 @@
+import { IoLocationOutline } from "react-icons/io5";
 import css from './Profile.module.css';
 import { ProfileListItem } from './Profile_list_item/Profile_list_item';
 
 export const Profile = ({ user: { username, tag, location, avatar, stats } }) => {
-    const statsArr = Array.from(stats)
-    return <div className='user-profile-container'>
+    const statsArr = Object.entries(stats)
+    return <div className={css.user_profile_container}>
        <img className={css.avatar} src={avatar} alt={username} width={30} height={30}/>
+        <p className={css.username}>{username}</p>
         <p className={css.tag}>@{tag}</p>
-        <p className={css.location}>{location}</p>
+        <div className={css.location_wrapper}>
+            <IoLocationOutline />
+            <p className={css.location}>{location}</p>
+        </div>        
         <ul className={css.stats_list}>
-            {statsArr.map(stat => {
-                console.log(stat);
-                return <ProfileListItem stat={stat} />
-            })}
-            
+            {statsArr.map(stat => <ProfileListItem key={stat[0]} stat={stat} />)}
         </ul>        
     </div>
 
--- src/components/Profile/Profile_list_item/Profile_list_item.jsx
+++ src/components/Profile/Profile_list_item/Profile_list_item.jsx
@@ -2,8 +2,8 @@ import css from './Profile_list_item.module.css';
 
 export const ProfileListItem = ({ stat }) => {
     console.log(stat);
-    return <li key={stat} className={css.stats_list_item}>
-        <p className={css.stats_list_item_title}>{stat}</p>
-        <p className={css.stats_list_item_value}>{stat.value}</p>
+    return <li className={css.stats_list_item}>
+        <p className={css.stats_list_item_title}>{stat[0]}</p>
+        <p className={css.stats_list_item_value}>{stat[1]}</p>
     </li>
 };    
