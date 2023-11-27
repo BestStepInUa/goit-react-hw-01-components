bf312e6d893bc912b09bcbf1cd6cab921a3d697a
BestStepInUa <beststep.in.ua@gmail.com>
2023-11-27 11:15:05 +0200

    some_fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,22 +2,26 @@ import PropTypes from 'prop-types';
 
 import css from './Statistics.module.css'
 
-export const Statistics = ({ data: { title, stats } }) => {
+export const Statistics = ({ title, data }) => {
+    console.log(data);
     return <section className={css.statistics}>
         {title && <h2 className={css.title}>Upload stats</h2>} 
 
         <ul className={css.stat_list}>
-            {stats.map(({ id, lable, percentage }) =>
-                <li key={id} className={css.item}>
+            {data.map(({ id, lable, percentage }) => {
+                return <li key={id} className={css.item}>
                     <span className={css.label}>{lable}</span>
                     <span className={css.percentage}>{percentage}</span>
                 </li>
-            )}
+            }
+                
+        )}
        </ul>
 </section>
 }
 
 Statistics.propTypes = {
+    title: PropTypes.string,
     data: PropTypes.array({
         id: PropTypes.string,
         label: PropTypes.string,
