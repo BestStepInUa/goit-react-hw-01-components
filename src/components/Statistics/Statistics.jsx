8743389caccbd21ff999834ed69b9703f2cc3e9c
BestStepInUa <beststep.in.ua@gmail.com>
2023-11-27 11:57:13 +0200

    task2_fixed_propTypes

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,28 +3,26 @@ import PropTypes from 'prop-types';
 import css from './Statistics.module.css'
 
 export const Statistics = ({ title, data }) => {
-    console.log(data);
     return <section className={css.statistics}>
         {title && <h2 className={css.title}>Upload stats</h2>} 
 
         <ul className={css.stat_list}>
-            {data.map(({ id, lable, percentage }) => {
-                return <li key={id} className={css.item}>
-                    <span className={css.label}>{lable}</span>
-                    <span className={css.percentage}>{percentage}</span>
-                </li>
-            }
-                
-        )}
+            {data.map(({ id, label, percentage }) => 
+                <li key={id} className={css.item}>
+                    <span className={css.label}>{label}</span>
+                    <span className={css.percentage}>{percentage}%</span>
+                </li> 
+            )}
        </ul>
 </section>
 }
 
 Statistics.propTypes = {
     title: PropTypes.string,
-    data: PropTypes.array({
+    data: PropTypes.arrayOf(
+        PropTypes.shape({
         id: PropTypes.string,
         label: PropTypes.string,
         percentage: PropTypes.number,
-    })
+    }))
 }
