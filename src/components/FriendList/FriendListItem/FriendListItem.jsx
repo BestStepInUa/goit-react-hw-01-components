1ab513873f185b2e8f29e3f2ab10bf2e70125872
BestStepInUa <beststep.in.ua@gmail.com>
2023-11-28 17:26:57 +0200

    task_3_refactoring_DONE

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -2,12 +2,12 @@ import PropTypes from 'prop-types';
 
 import css from './FriendListItem.module.css';
 
-export const FriendListItem = ({ avatar, name, isOnline }) =>
+export const FriendListItem = ({ friend: { avatar, name, isOnline } }) =>
     <li className={css.item}>
         {isOnline ? <span className={`${css.status} ${css.online}`}></span> : <span className={`${css.status} ${css.ofline}`}></span>}
         <img className={css.avatar} src={avatar} alt={name} width="48" />
         <p className={css.name}>{name}</p>
-    </li>
+    </li>   
 
 FriendListItem.protTypes = {    
     avatar: PropTypes.string,
